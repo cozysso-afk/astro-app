58da48c57a1e0055e68a701486b0c0da7cb83413
cozysso-afk <cozysso@gmail.com>
2026-08-27 00:35:42 +0900

    feat: add OneSignal web push service worker

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,3 @@
+// OneSignal Web SDK v16 service worker.
+// This file must stay publicly accessible on the same GitHub Pages origin.
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
