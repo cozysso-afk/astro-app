21bfc75de9e02f82eab964fffa709fd98323ef01
cozysso-afk <cozysso@gmail.com>
2026-08-29 16:44:17 +0900

    fix: load iPhone spacing correction

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -15,6 +15,7 @@ import './relationship-analysis.css'
 import './readability-v3.css'
 import './visual-overhaul-v5.css'
 import './visual-overhaul-v6.css'
+import './mobile-spacing-v11.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
