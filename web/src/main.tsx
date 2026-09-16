16e38b86bbc37865f64124b4e0083be2d8166431
cozysso-afk <cozysso@gmail.com>
2026-09-16 10:50:53 +0900

    style(home): load compatibility CTA layer

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -31,6 +31,7 @@ import './ios-viewport-stability.css'
 import './mobile-viewport-v31.css'
 import './vertical-density-v32.css'
 import './reading-type-hierarchy-v33.css'
+import './compatibility-cta-v34.css'
 import './reading-experience.css'
 
 installIntegratedPrecisionFetch()
