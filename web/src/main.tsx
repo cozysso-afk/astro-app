46a821f0a78d4460930d5fbf0f45f464d775ee7f
cozysso-afk <cozysso@gmail.com>
2026-09-16 14:54:06 +0900

    style(western): load independent reading UX layer

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -33,6 +33,7 @@ import './vertical-density-v32.css'
 import './reading-type-hierarchy-v33.css'
 import './home-information-architecture-v35.css'
 import './reading-experience.css'
+import './system-reading-ux-v40.css'
 
 installIntegratedPrecisionFetch()
 
